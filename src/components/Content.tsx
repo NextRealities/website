80f7865f191bcfd305eb8bb713e62c9077bf2e8b
Kenny Stanley <kpstanleyjr@gmail.com>
2021-12-30 07:58:02 -0600

    cleanup

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,6 @@
-export const Content = () => {
+export default function Content() {
     return (
-        <h2 className='text-2xl max-w-md mx-auto' data-testid="content">
+        <h2 className='text-2xl max-w-md mx-auto font-mono' data-testid="content">
             A simple Next.js application with TypeScript, Tailwind CSS and testing
             suite enabled
         </h2>
